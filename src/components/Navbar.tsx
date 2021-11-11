f0865a72cbe431f955a167a4f891a5a8e3d224e9
Joseph Whiteaker III <josephsims1@gmail.com>
2021-11-11 15:53:39 -0500

    add props to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 import React, { FC, ReactNode } from "react";
-
 interface NavbarProps {
     title?: string | ReactNode;
     icon?: ReactNode ; 
 
 }
 
-export const Navbar: FC<NavbarProps> = ({ title }) => {
+export const Navbar: FC<NavbarProps> = ({ title, icon }) => {
 
 
     return (
-        <div className=" h-24 bg-blue-600 min-w-full m-0 p-0  fixed">
+        <div className=" h-24 bg-blue-600 min-w-full m-0 p-0 fixed bg-opacity-100 ">
             <div className="flex-grow p-2 text-2xl flex items-center h-full text-white ">
+                    <span className="mx-2 "> 
+                        { icon}
+                    </span>
+                
                 {title || null}
             </div>
         </div>
