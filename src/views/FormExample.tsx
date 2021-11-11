c911b0582eb751a63e755ac42bd6d520709e8861
Joseph Whiteaker III <josephsims1@gmail.com>
2021-11-11 15:54:56 -0500

    partially scaffolded the form examples

--- src/views/FormExample.tsx
+++ src/views/FormExample.tsx
@@ -1,12 +1,146 @@
-import React , {FC, useEffect, useState} from "react";
-import {Navbar} from "@components/Navbar"
+import React, { FC, useEffect, useState } from "react";
+import { Navbar } from "@components/Navbar"
+import TextField from '@mui/material/TextField';
+import { MdClose } from "react-icons/md"
+import IconButton  from '@mui/material/IconButton';
+import { Link } from "react-router-dom";
 
-export const FormExample:FC<any> = () => {
+export const FormExample: FC<any> = () => {
 
-    const [loading , setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
 
-    return(
-        <Navbar title="hello world"/> 
+    
+
+    return (
+        <div className="overflow-y-scroll">
+
+            <Navbar title="hello world" icon={
+                <>
+                <Link to="">
+                    <IconButton>
+                        <MdClose className="text-white "/> 
+                    </IconButton>
+                    </Link>
+                </>
+            } />
+
+            <div className="p-2 m-2 flex flex-col my-36">
+                <h1 className=" text-blue-800 font-bold my-2 ">
+                    Hello
+                </h1>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+                <div className="flex flex-row my-2 ">
+                    <TextField
+                        required
+                        id="value"
+                        label="Required"
+
+                    />
+                </div>
+            </div>
+
+            <div className="flex flex-row justify-end">
+
+                <button className=" m-2 p-2  ">
+                    hello
+                </button>
+            </div>
+        </div>
     )
 }
