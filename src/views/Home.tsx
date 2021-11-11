50da4c2af5b994a8e22e925059014dbbf9256856
Joseph Whiteaker III <josephsims1@gmail.com>
2021-11-11 12:06:47 -0500

    Home component

---
+++ src/views/Home.tsx
@@ -0,0 +1,35 @@
+import { Loading } from "@components/Loading";
+import React, { useState, useEffect , FC} from "react";
+import { Route, Link} from "react-router-dom";
+
+
+export const Home:FC<any> = props => {
+
+
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
+
+
+    // Fake Loading 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 500)
+    }, [])
+
+    
+
+    return loading ? <Loading /> : (
+        <div className="flex flex-col justify-center items-center gap-4  min-h-screen">
+            <h1 className="text-lg md:text-3xl  m-3 p-3 max max-w-3xl">
+                Welcome to the Tailwind, Mobx, React Router v6, and Typescript proof of concept
+            </h1>
+            <div className="flex flex-row gap-3 m-3 items-center justify-evenly ">
+                <Link to="/example" >
+                    Form Example 
+                </Link>
+            </div>
+        </div>
+    )
+
+}
