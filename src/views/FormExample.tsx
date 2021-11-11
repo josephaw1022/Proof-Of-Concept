a740f553581708ab94821288150f18ae58c27711
Joseph Whiteaker III <josephsims1@gmail.com>
2021-11-11 16:11:08 -0500

    format file

--- src/views/FormExample.tsx
+++ src/views/FormExample.tsx
@@ -2,30 +2,33 @@ import React, { FC, useEffect, useState } from "react";
 import { Navbar } from "@components/Navbar"
 import TextField from '@mui/material/TextField';
 import { MdClose } from "react-icons/md"
-import IconButton  from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import { Link } from "react-router-dom";
-
+import { MdBackspace } from "react-icons/md"
 export const FormExample: FC<any> = () => {
 
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
 
-    
+
 
     return (
-        <div className="overflow-y-scroll">
+        <div className="overflow-y-scroll min-w-full overflow-x-hidden ">
 
-            <Navbar title="hello world" icon={
-                <>
-                <Link to="">
-                    <IconButton>
-                        <MdClose className="text-white "/> 
-                    </IconButton>
-                    </Link>
-                </>
-            } />
+            <Navbar
+                title="Form Example"
+                icon={
+                    <>
+                        <Link to="/">
+                            <IconButton className="m-0 p-0">
+                                <MdBackspace className="text-white " />
+                            </IconButton>
+                        </Link>
+                    </>
+                }
+            />
 
-            <div className="p-2 m-2 flex flex-col my-36">
+            <div className="p-2 m-2 flex flex-col my-36 min-w-full overflow-x-hidden ">
                 <h1 className=" text-blue-800 font-bold my-2 ">
                     Hello
                 </h1>
