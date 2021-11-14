b8642072b607d2d177bb5c6118b8acf6d017c082
Joseph Whiteaker III <josephsims1@gmail.com>
2021-11-14 16:03:32 -0500

    format navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import React, { FC, ReactNode } from "react";
+
+
+
 interface NavbarProps {
     title?: string | ReactNode;
-    icon?: ReactNode ; 
+    icon?: ReactNode;
 
 }
 
@@ -11,10 +14,9 @@ export const Navbar: FC<NavbarProps> = ({ title, icon }) => {
     return (
         <div className=" h-24 bg-blue-600 min-w-full m-0 p-0 fixed bg-opacity-100 ">
             <div className="flex-grow p-2 text-2xl flex items-center h-full text-white ">
-                    <span className="mx-2 "> 
-                        { icon}
-                    </span>
-                
+                <span className="mx-2 ">
+                    {icon}
+                </span>
                 {title || null}
             </div>
         </div>
